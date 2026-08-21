d5d14d077166f890485fdbc3501c9e100fd31637
fernando villamor jr <fervillz@gmail.com>
2026-08-21 08:20:45 +0800

    Bump Quote System version for JSON setup data

--- quote-system.php
+++ quote-system.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Quote System
  * Description: Frontend quotation system for Loughlin Furniture.
- * Version: 1.6.0
+ * Version: 1.6.1
  * Author: Loughlin Furniture
  * Text Domain: quote-system
  */
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin Constants
  */
-define( 'QS_VERSION', '1.6.0' );
+define( 'QS_VERSION', '1.6.1' );
 define( 'QS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'QS_URL', plugin_dir_url( __FILE__ ) );
 
